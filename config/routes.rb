9b55e10e16e930989c649d2bc05655125a4a2ac5
関優花 <skyk@MacBook-Air-3.local>
2024-06-14 16:25:43 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  # # root "application#hello" # 上記に差し替え
+  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # # Defines the root path route ("/")
+  # # root "posts#index"
 end
