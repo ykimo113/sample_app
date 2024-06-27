099c64895523540c08db1e6c37b1b33d7bd771fb
関優花 <skyk@MacBook-Air-3.local>
2024-06-27 14:24:15 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get "/help", to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
   # root "application#hello" # 上記に差し替え
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
