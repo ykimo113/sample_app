2a1a157e07edc3533be6948a8c8381dee376e25b
関優花 <skyk@MacBook-Air-3.local>
2024-07-04 00:57:12 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,5 @@ Rails.application.routes.draw do
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
-  # root "application#hello" # 上記に差し替え
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :users
 end
