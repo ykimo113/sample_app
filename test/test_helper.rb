d8b1911b135e9594f44407af5e3c5d8758b9eac9
関優花 <skyk@MacBook-Air-3.local>
2024-07-05 12:16:30 +0900

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,15 +4,29 @@ require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+# module ActiveSupport
+#   class TestCase
+#     # Run tests in parallel with specified workers
+#     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-    include ApplicationHelper
+#     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+#     fixtures :all
+#     include ApplicationHelper
 
-    # Add more helper methods to be used by all tests here...
+#     # Add more helper methods to be used by all tests here...
+#   end
+# end
+
+class ActiveSupport::TestCase
+  # 指定のワーカー数でテストを並列実行する
+  parallelize(workers: :number_of_processors)
+  # test/fixtures/*.ymlのfixtureをすべてセットアップする
+  fixtures :all
+  # これがないと"full_title"が認識されない
+  include ApplicationHelper
+
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
   end
 end
