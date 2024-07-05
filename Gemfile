3360da486f4732cf3c2cb8cd200c7cdd5e3d468f
関優花 <skyk@MacBook-Air-3.local>
2024-07-06 02:36:43 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -35,6 +35,11 @@ gem "jbuilder"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "3.1.18"
 
+# 第10章で追加しているものたち
+gem "faker",           "2.21.0"
+gem "will_paginate",           "3.3.1"
+gem "bootstrap-will_paginate", "1.0.0"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,16 +20,20 @@ module SessionsHelper
         if (user_id = session[:user_id])
             user = User.find_by(id: user_id)
             if user && session[:session_token] == user.session_token
-                @current_user = user
+              @current_user = user
             end
         elsif (user_id = cookies.encrypted[:user_id])
-            # raise       # テストがパスすれば、この部分がテストされていないことがわかる
             user = User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
-                log_in user
-                @current_user = user
+              log_in user
+              @current_user = user
             end
         end
+    end
+
+    # 渡されたユーザーがカレントユーザーであればtrueを返す
+    def current_user?(user)
+        user && user == current_user
     end
 
     # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -51,4 +55,8 @@ module SessionsHelper
         @current_user = nil   # 安全のため
     end
 
+    # アクセスしようとしたURLを保存する
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
 end
