212a9190487917f88ce9cb6141a0903e629d08e0
関優花 <skyk@MacBook-Air-3.local>
2024-07-07 01:40:43 +0900

    finish Rails Tutorial Section 14

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -34,7 +34,7 @@ class FollowPagesTest < Following
   test "feed on Home page" do
     get root_path
     @user.feed.paginate(page: 1).each do |micropost|
-      assert_match CGI.escapeHTML(（コードを書き込む）), （コードを書き込む）
+      assert_match CGI.escapeHTML(micropost.content), response.body
     end
   end
 end
