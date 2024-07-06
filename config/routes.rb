5d1de77bc5a9ca6a32df9c44e73f8514b515343a
関優花 <skyk@MacBook-Air-3.local>
2024-07-07 01:32:37 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,15 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # 一部のブラウザでの画面再読み込みエラーを修正
   get '/microposts', to: 'static_pages#home'
 end
